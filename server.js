5f6287d9ade5ebb6008c2cdd79b30480a413010b
Gus Caplan <me@gus.host>
2017-10-21 16:57:37 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const http = require('http');
+const fs = require('fs');
+const request = require('snekfetch');
+
+const template = fs.readFileSync('./template.sh').toString();
+const t = (v, c) => {
+  console.log(`Generating ${c}/${v}`); // eslint-disable-line no-console
+  return template
+    .replace(/{{VERSION}}/g, `v${v.replace('v', '')}`)
+    .replace(/{{CHANNEL}}/g, c);
+};
+
+const vcache = {};
+
+async function ensureVersion(channel) {
+  const i = vcache[channel];
+  if (!i || (i && Date.now() - i.time > 600000)) {
+    const url = channel === 'nightly' ?
+      'https://nodejs.org/download/nightly/index.json' :
+      'https://nodejs.org/dist/index.json';
+    const versions = await request.get(url).then((r) => r.body);
+    switch (channel) {
+      case 'latest':
+      case 'nightly': {
+        const version = versions[0].version;
+        vcache[channel] = { time: Date.now(), version };
+        return version;
+      }
+      case 'lts': {
+        const version = versions.find((v) => v.lts).version;
+        vcache.lts = { time: Date.now(), version };
+        return version;
+      }
+    }
+  } else {
+    return i.version;
+  }
+}
+
+const server = http.createServer(async(req, res) => {
+  const url = req.url.slice(1);
+  switch (url) {
+    case '':
+      res.end('hi');
+      break;
+    case 'latest':
+    case 'lts':
+    case 'nightly': {
+      const version = await ensureVersion(url);
+      res.end(t(version, url === 'nightly' ? 'nightly' : 'release'));
+      break;
+    }
+    default:
+      res.end(t(url, 'release'));
+  }
+});
+
+server.listen(1337);
---
+++ template.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+cd $(mktemp -d)
+
+OS_TYPE=$(uname | tr '[:upper:]' '[:lower:]')
+OS_ARCH=$(uname -m)
+if [ $OS_ARCH = "x86_64" ]; then OS_ARCH="x64"; fi
+
+ACH_NAME="node-{{VERSION}}-$OS_TYPE-$OS_ARCH"
+ACH_URL="https://nodejs.org/download/{{CHANNEL}}/{{VERSION}}/$ACH_NAME.tar.gz"
+
+echo "Downloading $ACH_URL"
+curl -# -O $ACH_URL
+
+echo "Extracting $ACH_NAME.tar.gz"
+if hash pv 2>/dev/null; then
+  pv -p -w 80 "$ACH_NAME.tar.gz" | tar -xf -
+else
+  tar -xf "$ACH_NAME.tar.gz"
+fi
+
+cd $ACH_NAME
+find . -maxdepth 1 -type f -delete
+
+cp -r * /usr/local
+
+if hash node 2>/dev/null; then
+  echo "Node.js {{VERSION}} has been installed"
+else
+  echo "Node.js install failed!"
+  exit 1
+fi
